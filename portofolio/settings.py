6bdca9cfac31f8c11b3dc57488ebf89b48cf3b20
tipen7 <tipdyanizha@gmail.com>
2026-09-01 15:28:00 +0700

    chore: added static files configuration in settings.py

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -138,6 +138,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
